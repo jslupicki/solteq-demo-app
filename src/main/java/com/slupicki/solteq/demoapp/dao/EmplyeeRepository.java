7629f5a30a030686702f7f5cf1f8fe00820b5af3
jslupicki <jan@slupicki.com>
2018-06-22 20:29:06 +0200

    PagingAndSortingRepository is better as give sorting capability "for free"

--- src/main/java/com/slupicki/solteq/demoapp/dao/EmplyeeRepository.java
+++ src/main/java/com/slupicki/solteq/demoapp/dao/EmplyeeRepository.java
@@ -1,9 +1,9 @@
 package com.slupicki.solteq.demoapp.dao;
 
 import com.slupicki.solteq.demoapp.model.Employee;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
-public interface EmplyeeRepository extends CrudRepository<Employee, UUID> {
+public interface EmplyeeRepository extends PagingAndSortingRepository<Employee, UUID> {
 }
--- src/main/java/com/slupicki/solteq/demoapp/dao/SalaryRepository.java
+++ src/main/java/com/slupicki/solteq/demoapp/dao/SalaryRepository.java
@@ -1,9 +1,9 @@
 package com.slupicki.solteq.demoapp.dao;
 
 import com.slupicki.solteq.demoapp.model.Salary;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
-public interface SalaryRepository extends CrudRepository<Salary, UUID> {
+public interface SalaryRepository extends PagingAndSortingRepository<Salary, UUID> {
 }
