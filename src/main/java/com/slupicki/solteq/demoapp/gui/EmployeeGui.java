587da1029da2bcafb97e48d822d8b1c5b32e97ab
jslupicki <jan@slupicki.com>
2018-06-22 17:14:29 +0200

    Move edit and add employee to modal window

--- src/main/java/com/slupicki/solteq/demoapp/gui/EmployeeGui.java
+++ src/main/java/com/slupicki/solteq/demoapp/gui/EmployeeGui.java
@@ -10,14 +10,14 @@ import com.vaadin.server.VaadinServlet;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ValoTheme;
+import io.vavr.control.Try;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.annotation.WebServlet;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.text.DecimalFormat;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -33,31 +33,29 @@ public class EmployeeGui extends UI {
 
     private List<Employee> employees;
 
+    private final Grid<Employee> employeeGrid = new Grid<>();
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         employees = iterableToList(repository.findAll());
 
         final VerticalLayout layout = new VerticalLayout();
-        final Grid<Employee> employeeGrid = new Grid<>();
 
-        final TextField firstNameTf = new TextField();
-        firstNameTf.setCaption("First name:");
-        final TextField lastNameTf = new TextField();
-        lastNameTf.setCaption("Last name:");
-
+        final Label employeesLbl = new Label("Employees");
         Button addBtn = new Button("Add");
         addBtn.addClickListener(e -> {
             Employee employee = new Employee(
-                    firstNameTf.getValue(),
-                    lastNameTf.getValue(),
+                    "",
+                    "",
                     BigDecimal.ZERO
             );
-            repository.save(employee);
-            refreshGrid(employeeGrid);
-            firstNameTf.setValue("");
-            lastNameTf.setValue("");
-            layout.addComponent(new Label("You have " + repository.count() + " employees"));
+            editEmployee(employee);
         });
+
+        HorizontalLayout horizontalLayout = new HorizontalLayout(
+                employeesLbl,
+                addBtn
+        );
 
         employeeGrid.addColumn(employee -> employees.indexOf(employee) + 1).setExpandRatio(0);
         employeeGrid.addColumn(Employee::getFirstName).setCaption("First name").setExpandRatio(1);
@@ -67,9 +65,7 @@ public class EmployeeGui extends UI {
         employeeGrid.addComponentColumn(this::deleteButton).setExpandRatio(0);
         employeeGrid.setFrozenColumnCount(1);
 
-        layout.addComponent(firstNameTf);
-        layout.addComponent(lastNameTf);
-        layout.addComponent(addBtn);
+        layout.addComponent(horizontalLayout);
         layout.addComponent(employeeGrid);
 
         setContent(layout);
@@ -85,7 +81,48 @@ public class EmployeeGui extends UI {
     }
 
     private void editEmployee(Employee e) {
-        log.info("Edit: {}", e);
+        Window window = new Window();
+        window.setWidth(300.0f, Unit.PIXELS);
+        final FormLayout content = new FormLayout();
+        content.setMargin(true);
+        final TextField firstNameTf = new TextField();
+        firstNameTf.setCaption("First name:");
+        firstNameTf.setValue(e.getFirstName());
+        final TextField lastNameTf = new TextField();
+        lastNameTf.setCaption("Last name:");
+        lastNameTf.setValue(e.getLastName());
+        final TextField salaryTf = new TextField();
+        salaryTf.setCaption("Salary:");
+        salaryTf.setValue(String.valueOf(e.getSalary()));
+        Button saveBtn = new Button("Save", event -> {
+            log.info("Save edit: {}", e);
+            e.setFirstName(firstNameTf.getValue());
+            e.setLastName(lastNameTf.getValue());
+            DecimalFormat decimalFormat = new DecimalFormat();
+            decimalFormat.setParseBigDecimal(true);
+            BigDecimal salary = Try.of(() -> (BigDecimal) decimalFormat.parse(salaryTf.getValue())).getOrElse(BigDecimal.ZERO);
+            e.setSalary(salary);
+            repository.save(e);
+            refreshGrid();
+            window.close();
+        });
+        Button cancelBtn = new Button("Cancel", event -> {
+            log.info("Cancel edit: {}", e);
+            window.close();
+        });
+        final HorizontalLayout buttonLayout = new HorizontalLayout(
+                cancelBtn,
+                saveBtn
+        );
+        content.addComponent(firstNameTf);
+        content.addComponent(lastNameTf);
+        content.addComponent(salaryTf);
+        content.addComponent(buttonLayout);
+        window.setContent(content);
+        window.setModal(true);
+        window.setClosable(false);
+        window.setResizable(false);
+        this.addWindow(window);
     }
 
     private Button deleteButton(Employee e) {
@@ -96,13 +133,31 @@ public class EmployeeGui extends UI {
     }
 
     private void deleteEmployee(Employee e) {
-        Window window = new Window("Test");
-        window.setWidth(300.0f, Unit.PIXELS);
-        final FormLayout content = new FormLayout();
+        Window window = new Window("Are you sure?");
+        //window.setWidth(300.0f, Unit.PIXELS);
+        final HorizontalLayout content = new HorizontalLayout();
         content.setMargin(true);
+        Button yesBtn = new Button("Yes", event -> {
+            log.info("Delete: {}", e);
+            repository.delete(e);
+            refreshGrid();
+            window.close();
+        });
+        Button noBtn = new Button("No", event -> {
+            log.info("NO delete: {}", e);
+            window.close();
+        });
+        content.addComponent(noBtn);
+        content.addComponent(yesBtn);
         window.setContent(content);
+        window.setModal(true);
+        window.setClosable(false);
+        window.setResizable(false);
         this.addWindow(window);
-        log.info("Delete: {}", e);
+    }
+
+    private void refreshGrid() {
+        refreshGrid(employeeGrid);
     }
 
     private void refreshGrid(Grid<Employee> grid) {
@@ -112,6 +167,7 @@ public class EmployeeGui extends UI {
 
     private void refreshGrid(Grid<Employee> grid, List<Employee> employees) {
         grid.setItems(employees);
+        grid.recalculateColumnWidths();
     }
 
     private <T> List<T> iterableToList(Iterable<T> iterable) {
